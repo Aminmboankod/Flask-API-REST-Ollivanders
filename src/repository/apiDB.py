03c6016a364bdf065f53b860882e9ced80676171
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-03-17 17:15:48 +0100

    feat(read_item)

--- src/repository/apiDB.py
+++ src/repository/apiDB.py
@@ -1,15 +1,17 @@
 import requests
 import json
-import os
 
 
 
 
 
-# Get Headers devuelve el diccionario 
-# con el encabezado para las solicitudes
+
 
 def get_headers():
+    """
+        Este método contiene el encabezado para las peticiones
+        Return: El encabezado para ser añadido en las peticiones 
+    """
     return {
         'Content-Type': 'application/json',
         'Access-Control-Request-Headers': '*',
@@ -18,6 +20,11 @@ def get_headers():
 
 
 def read_items():
+    """
+        Este método hace una petición POST a la base de datos sin filtro
+        Return: Toda la información que contiene la base de datos en formato JSON
+    """
+
     url = "https://eu-west-2.aws.data.mongodb-api.com/app/data-qcybd/endpoint/data/v1/action/find"
     payload = json.dumps({
         "collection": "items",
@@ -28,3 +35,18 @@ def read_items():
 
     response = requests.request("POST", url, headers=get_headers(), data=payload)
     return response
+
+
+def read_item(item_name):
+    url = "https://eu-west-2.aws.data.mongodb-api.com/app/data-qcybd/endpoint/data/v1/action/find"
+    payload = json.dumps({
+        "collection": "items",
+        "database": "Ollivanders-shop",
+        "dataSource": "OllivandersCluster",
+        "filter": {"name": item_name},
+    })
+    response = requests.request("POST", url, headers=get_headers(), data=payload)
+
+    response = json.loads(response.text)
+    item = response.get('documents')
+    return item
--- test/unitary/repository/test_apiDB.py
+++ test/unitary/repository/test_apiDB.py
@@ -1,9 +1,15 @@
-from src.repository.apiDB import read_items
+from src.repository.apiDB import read_items, read_item
 
+import json
 import pytest
 
 @pytest.mark.connectionDatabase
 def test_conection_db():
 
     assert read_items() != None
-    assert read_items().status_code == 200 
+    assert read_items().status_code == 200
+    
+
+def test_read_item():
+
+    assert read_item("+5 Dexterity Vest") == [{"_id": "64124bcb1d1c607ae6239ddd", "name": "+5 Dexterity Vest", "quality": 20, "sell_in": 10}]
